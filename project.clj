a30e59d3961b93d5719be3bfc30d3ad33ddbf844
Sean Corfield <sean@corfield.org>
2017-10-25 19:44:20 -0700

    Updated to Expectations 2.2.0-rc2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [expectations "2.2.0-beta4"]]
+                 [expectations "2.2.0-rc2"]]
   :main ^:skip-aot testing.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
